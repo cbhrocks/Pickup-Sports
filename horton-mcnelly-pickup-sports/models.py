722d1e162cfca6fea3d4d6aa8a2b633951322688
Charles Horton <charlie@horton5.com>
2015-08-09 13:18:47 -0700

    added api file

--- horton-mcnelly-pickup-sports/models.py
+++ horton-mcnelly-pickup-sports/models.py
@@ -4,4 +4,13 @@ Created on Aug 8, 2015
 @author: Charlie
 '''
 from endpoints_proto_datastore.ndb.model import EndpointsModel
-from google.appengine.ext import ndbv
+from google.appengine.ext import ndb
+from google.appengine.api.validation import Repeated
+
+class Profile(EndpointsModel):
+    _message_fields_schema = ("entityKey", "first_name", "last_name", "email", "phone_number", "friends")
+    first_name=ndb.StringProperty
+    last_name=ndb.StringProperty
+    email=ndb.StringProperty
+    phone_number=ndb.IntegerProperty
+    friends=ndb.KeyProperty(kind="Profile", Repeated=False)
